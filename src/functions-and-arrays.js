98d6637cae4d57ba10952a99f79906920ec889b8
Swapnil Dekate <smdekate.pro@gmail.com>
2025-07-14 06:12:47 +0000

    Iteration #2: Complete

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -11,7 +11,19 @@ function maxOfTwoNumbers(a, b) {
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  if (words.length === 0) return null;
+
+  let longestWord = words[0];
+
+  for (let i = 1; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+
+  return longestWord;
+}
 
 
 
